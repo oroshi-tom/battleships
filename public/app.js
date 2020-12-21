09afd0bed2c18665e16255ed7a3550305644d61e
oroshi-tom <orosh1@protonmail.com>
2020-12-20 20:19:27 -0700

    turns using synchronous functions

--- public/app.js
+++ public/app.js
@@ -1,36 +1,25 @@
-// Get Dom elements
-//#################
+//####################################################################//
+// Get DOM Elements
+//####################################################################//
 const grid = document.querySelector(".grid");
 const cells = [];
 const topBar = document.querySelector(".top-bar");
 const sideBar = document.querySelector(".side-bar");
-// const destroyerBtn = document.querySelector(".destroyer-btn");
-// const cruiserBtn = document.querySelector(".cruiser-btn");
-
-// const startBtn = document.querySelector(".start-btn");
 const readyBtn = document.querySelector(".ready-btn");
 const turnBtn = document.querySelector(".turn-btn");
 const messageDisplay = document.querySelector(".message");
 const currtentPlayerDisplay = document.querySelector(".player-Id");
 
-//--------------------------------------------------------------------//
-
+//####################################################################//
 // Declare Globals
-//################
+//####################################################################//
 const topBarLabels = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 const height = 10;
 const width = 10;
 
-//// Current Player - set to one
-var currentPlayer = 1;
-
-// Placeholders for ship co-ordinates
-const playerOneShips = [];
-const playerTwoShips = [];
-//--------------------------------------------------------------------//
-
+//####################################################################//
 // Create Board
-//#############
+//####################################################################//
 function createBoard() {
   // Create top-bar
   for (let i = 0; i < topBarLabels.length; i++) {
@@ -45,7 +34,8 @@ function createBoard() {
     row.textContent = i;
     sideBar.appendChild(row);
   }
-  // create Grid
+
+  //create Grid
   for (let i = 0; i < height * width; i++) {
     var cell = document.createElement("div");
     // Give each cell a unique id
@@ -57,21 +47,20 @@ function createBoard() {
 }
 
 createBoard();
-//--------------------------------------------------------------------//
 
-// Define Classes
-let turn = true;
-let message;
-
-//// Player Class
+//####################################################################//
+// Classes
+//####################################################################//
 class Player {
   constructor(id) {
     this.id = id;
   }
+  // Each player has ships, shots taken, and hits on thier own ship
   ships = [];
   shots = [];
   hits = [];
-  // Place ships on board
+
+  // PlaceShips Method
   placeShips(player) {
     message =
       "<h2>" +
@@ -82,22 +71,97 @@ class Player {
       "Place your ships by clicking on the grid, click 'Ready' when finished" +
       "</p>";
     messageDisplay.innerHTML = message;
-    let cellId;
-    // Listen to grid for clicks
+    let currentCellId;
+    // Listen for clicks on grid, only run once
+    grid.addEventListener(
+      "click",
+      (e) => {
+        // Get the id of the cell
+        currentCellId = parseInt(e.target.dataset.id);
 
-    grid.addEventListener("click", (e) => {
-      // Color the background
-      e.target.style.background = "gray";
-      // Get the selected cell's id's
-      cellId = parseInt(e.target.dataset.id);
-      // Store id's into instance's ships array
-      this.ships.push(cellId);
-    });
-    // Increment ready count
-    readyCount++;
+        // Color the cell
+        cells[currentCellId].style.background = "gray";
+
+        // Define next valid cells, based on first cell
+        let validCells = [
+          // Cell directly above
+          currentCellId - 10,
+          // Cell directly below
+          currentCellId + 10,
+          // Cell after
+          currentCellId + 1,
+          // Cell before
+          currentCellId - 1,
+        ];
+
+        // highlight valid cells if they are on grid
+        for (let cell of validCells) {
+          if (cell > 0) {
+            cells[cell].style.background = "green";
+            cells[cell].style.opacity = 0.5;
+          }
+        }
+
+        // Listen for second clicks on grid
+        grid.addEventListener(
+          "click",
+          (e) => {
+            // Remove valid cell highlighting
+            for (let cell of validCells) {
+              if (cell > 0) {
+                if ((cells[cell].style.background = "green")) {
+                  cells[cell].style.background = "";
+                  cells[cell].style.opacity = 1;
+                }
+              }
+            }
+            // get id of second cell
+            let nextCellId = parseInt(e.target.dataset.id);
+
+            // Check if cell is included in validCells
+            let validCell = validCells.includes(nextCellId);
+
+            // If second cell is valid
+            if (validCell) {
+              // Color the cell gray
+              cells[nextCellId].style.background = "gray";
+
+              // Get direction, and fill in the rest of the ship
+              switch (nextCellId) {
+                // Cell directly above
+                case currentCellId - 10:
+                  // color the block @ current - 20
+                  cells[currentCellId - 20].style.background = "gray";
+                  break;
+                // color the block @ current + 20
+                case currentCellId + 10:
+                  cells[currentCellId + 20].style.background = "gray";
+                  break;
+                // color the block @ current + 20
+                case currentCellId + 1:
+                  cells[currentCellId + 2].style.background = "gray";
+                  break;
+                // color the block @ current - 2
+                case currentCellId - 1:
+                  cells[currentCellId - 2].style.background = "gray";
+                  break;
+              }
+              cells.forEach((cell) => {
+                if (cell.style.background == "gray") {
+                  this.ships.push(cell.dataset.id);
+                }
+              });
+              console.log(`player 1 ships: ${player_1.ships}`);
+              console.log(`player 2 ships: ${player_2.ships}`);
+            }
+          },
+          { once: true }
+        );
+      },
+      { once: true }
+    );
   }
-
-  // Place shots on board
+  // TakeShots Method
   takeShot(player) {
     message =
       "<h2>" + "Player " + this.id + "</h2>" + "<p>" + "Your Turn" + "</p>";
@@ -110,28 +174,36 @@ class Player {
         e.target.style.background = "red";
         cellId = parseInt(e.target.dataset.id);
         this.shots.push(cellId);
+        console.log(`Player ${this.id} fired on cell: ${cellId}`);
         // Check if shot matches other player ship location
         // If player 1
-        if (turn) {
-          if (game.player_2.ships.includes(cellId)) {
+        if (this.id == 1) {
+          if (player_2.ships.includes(cellId)) {
             this.hits.push(cellId);
+            alert("hit!");
+          } else {
+            alert("miss");
           }
           // If player 2
         } else {
-          if (game.player_1.ships.includes(cellId)) {
+          if (player_1.ships.includes(cellId)) {
             this.hits.push(cellId);
+            alert("hit!");
+          } else {
+            alert("miss");
           }
         }
-
-        console.log(`player 1 hits: ${game.player_1.hits}`);
-        console.log(`player 2 hits: ${game.player_2.hits}`);
       },
       { once: true }
     );
+    console.log(`player 1 ships left: ${player_1.ships.length}`);
+    console.log(`player 2 ships left: ${player_2.ships.length}`);
+    console.log(`player 1 ships hit: ${player_1.hits.length}`);
+    console.log(`player 2 ships hit: ${player_2.hits.length}`);
   }
-  turn = !turn;
 }
 
+// Game class
 class Game {
   constructor(player_1, player_2) {
     this.player_1 = player_1;
@@ -139,22 +211,28 @@ class Game {
   }
 }
 
-// START GAME
-//#################
-
-// Initialize game + players
+//####################################################################//
+// Instantiate players and game
+//####################################################################//
 var player_1 = new Player(1);
 var player_2 = new Player(2);
 var game = new Game(player_1, player_2);
+
+//####################################################################//
+// Start Game
+//####################################################################//
 let readyCount = 0;
-let ready = false;
-let win = false;
+let message;
+let turn = true;
+let gameOver;
 
-game.player_1.placeShips();
+while (readyCount < 2) {
+  //// Place ships first
+  game.player_1.placeShips();
 
-// Place player 1 ships on board
-readyBtn.addEventListener("click", playerReady);
-function playerReady() {
+  // Increment ready count
+  readyCount++;
+
   // If player one is ready
   if (readyCount == 1) {
     // Hide player 1 ships
@@ -171,36 +249,21 @@ function playerReady() {
     game.player_2.ships.forEach((ship) => {
       cells[ship].style.background = "";
     });
-    // stop listening to ready button
-    readyBtn.removeEventListener("click", playerReady);
-    readyBtn.classList.toggle("hidden");
-    while (true) {
-      startTurns();
-      if (win) {
-        break;
-      }
-    }
   }
-  // Console info for debugging
-  console.log(`player 1 ships: ${game.player_1.ships}`);
-  console.log(`player 2 ships: ${game.player_2.ships}`);
-  console.log(`ready count: ${readyCount}`);
 }
 
+// Start game turns
 function startTurns() {
   // Check for winner
-  if (
-    game.player_1.ships.length != 0 &&
-    game.player_1.ships.length == game.player_1.hits.length
-  ) {
-    win = true;
+  while (true) {
+    if (turn) {
+      game.player_1.takeShot();
+    } else {
+      game.player_2.takeShot();
+      break;
+    }
+    turn = !turn;
   }
-  game.player_1.takeShot();
-
-  if (turn) {
-    game.player_1.takeShot();
-  } else {
-    game.player_2.takeShot();
-  }
-  turn = !turn;
 }
+
+startTurns();
